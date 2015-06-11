8c9f7fcd712c3d18623b1d35f532126195a72fb0
Jeffrey Moon <Moon.JeffreyW@gmail.com>
2015-06-10 21:57:00 -0500

    Reorganized globals, fixed subtractor

---
+++ src/Globals.h
@@ -0,0 +1,14 @@
+#ifndef GLOBALS_H_
+#define GLOBALS_H_
+
+#define WORDSIZE 16
+#define BYTESIZE 8
+
+#define MAXWORDSIZE_U 65535
+#define MAXWORDSIZE 32767
+#define MINWORDSIZE -32768
+
+typedef unsigned char byte;
+typedef unsigned short word;
+
+#endif
